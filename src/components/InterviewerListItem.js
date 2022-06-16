50649b5f5e77d4e51dbfda79ffc961041dfd74e1
Ali <alihbouran@gmail.com>
2022-06-16 13:02:12 -0700

    Add appointment directory and test

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,7 +10,7 @@ export default function InterviewerListItem(props) {
   })
 
   return (
-    <li className={interviewerClass} key={props.id} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={props.setInterview}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
