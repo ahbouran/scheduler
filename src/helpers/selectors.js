654747f60b93180020571e17ab69817f27496d31
Ali <alihbouran@gmail.com>
2022-06-17 19:27:21 -0700

    Create helper/selector function to get necessary appointments for a given day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,21 @@
 export function getAppointmentsForDay(state, day) {
-  //... returns an array of appointments for that day
+  if (state === undefined || day === undefined) {
+    return []
+  }
+  let selectedDays = [];
+  let finalArray = [];
+  
+  for (let days of state.days) {
+    if (days.name === day) {
+      selectedDays = [...days.appointments]
+    }
+  }
+
+  for (let index of selectedDays) {
+    finalArray.push(state.appointments[index])
+  }
+
+
+return finalArray
+
 }
